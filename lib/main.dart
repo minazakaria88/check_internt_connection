7ac3f1d0d3978be983014128c87460f3c087f05f
App <mynaz7142@gmail.com>
2024-11-13 12:28:01 +0200

    add sized box

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,41 @@ class CheckInternetConnection extends StatelessWidget {
             body: Center(
               child: Column(
                 children: [
-                  Builder(
-                    builder: (context) {
-                      return Container(
-                        margin: const EdgeInsets.all(50),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: const Color(0xFF3562D3),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 2,
-                                offset: const Offset(
-                                    0, 1), // changes position of shadow
-                              ),
-                            ]),
-                        child: MaterialButton(
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SecondScreen(),));
-                          },
-                          child: const Text(
-                            'Sign In',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Builder(builder: (context) {
+                    return Container(
+                      margin: const EdgeInsets.all(50),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0xFF3562D3),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 2,
+                              offset: const Offset(
+                                  0, 1), // changes position of shadow
                             ),
+                          ]),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const SecondScreen(),
+                          ));
+                        },
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
-                      );
-                    }
-                  ),
+                      ),
+                    );
+                  }),
                 ],
               ),
             ),
